d82cc4b1880d3b3a426f6f8eda47b5b6756a7d02
EvalPutraParasdika <evalputra0@gmail.com>
2026-06-03 19:13:01 +0700

    Rename ProtectedRoute

--- src/components/protectedRoute.jsx
+++ src/components/protectedRoute.jsx
@@ -3,12 +3,12 @@ import { Navigate } from "react-router-dom";
 const ProtectedRoute = ({ children }) => {
   const token = localStorage.getItem("token");
 
-  // Kalau belum login
+  
   if (!token) {
     return <Navigate to="/login" replace />;
   }
 
-  // Kalau sudah login
+  
   return children;
 };
 
